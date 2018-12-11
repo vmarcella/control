83c8ec6d4e00994d6320f77db8edaa90fb3a5cea
C3NZ <cenz@cenz.io>
2018-12-10 21:07:49 -0800

    [feat] create user model

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+
+    description: {
+        type: String,
+    },
+
+    companiesCreated: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Company'
+    }],
+
+    createdAt: {
+        type: Date,
+    },
+
+    updatedAt: {
+        type: Date,
+    }
+});
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    const now = new Date();
+
+    user.updatedAt = now;
+    if (!user.createdAt) {
+        user.createdAt = now;
+    }
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) return next(err);
+
+                user.password = hash;
+                return next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+userSchema.methods.comparePassword = function(password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        return done(err, isMatch);
+    })
+}
+
+module.exports = mongoose.model('User', userSchema);
