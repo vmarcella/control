cfe0e060f0abc26f7eddb9af748c87975635f992
C3NZ <cenz@cenz.io>
2018-12-10 21:02:37 -0800

    [feat] add auth router to app and instantiate db connection

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
+// Import npm modules and std lib dependencies
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Configure our dotenv
+require('dotenv').config();
+
+// Import custom routes from routes folder
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 const apiRouter = require('./routes/api');
 
+// Instantiate express app
 const app = express();
+
+// Create the database connection
+const dbConnection = require('./data/control-db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Register middleware before any request hits our routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Register custom routes
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
