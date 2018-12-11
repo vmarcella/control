1853038af9cd9a1cb52097579ae8b5f8b4635a76
C3NZ <cenz@cenz.io>
2018-12-10 21:03:05 -0800

    [feat] create company schema

--- models/company.js
+++ models/company.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+
 const { Schema } = mongoose;
 
 const companySchema = new Schema({
+    name: { type: String, required: true, unique: true },
+    verified: { type: Boolean },
+    description: { type: String, default: '' },
+    employees: [{ type: String, default: [] }],
+});
 
-})
+module.exports = mongoose.model('Company', companySchema)
