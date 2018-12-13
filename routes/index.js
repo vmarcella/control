119933d8916d15c3aed0b5b67d8981acb72f82bc
C3NZ <cenz@cenz.io>
2018-12-12 21:58:58 -0800

    [feat] render views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Control API', indexActive: 'active' });
 });
 
+// Get the documentation for the API
+router.get('/documentation', (req, res) => {
+    res.render('documentation', { title: 'Control API', docsActive: 'active' })
+})
 module.exports = router;
